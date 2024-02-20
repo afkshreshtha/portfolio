754c5bc707c15f1f87f693434fe0cc0411180a35
afkshreshtha <shreshtha1345@gmail.com>
2024-02-20 10:20:37 +0530

    add google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta name="google-site-verification" content="QibisYWcAxZwed8-_w-2OVseaL58KAxSe7Jb2A6wXLQ" />
       <body className={`${inter.className} scroll-smooth   overflow-x-hidden bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}>
         <div className="bg-[#FBE2E3] absolute top-[-6rem] -z-10  right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35remrem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
