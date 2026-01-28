b067795b116471ed0563ed60a9cf8f30ab970170
afkshreshtha <shreshthagarwal1234@gmail.com>
2026-01-28 15:10:19 +0530

    g

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,19 +1,5 @@
-function assertValue<T>(v: T | undefined, errorMessage: string): T {
-  if (v === undefined) {
-    throw new Error(errorMessage);
-  }
-  return v;
-}
-
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2026-01-28";
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-04-08";
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET"
-);
-
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
-);
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "5uvz6jlv";
